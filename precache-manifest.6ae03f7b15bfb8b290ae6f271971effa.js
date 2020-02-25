17a1fec1f51d13e41c2d3f283ac9f74e52abb7a5
Anton Cheng <anton@coolbitx.com>
2020-02-26 06:40:27 +0800

    Updates

---
+++ precache-manifest.6ae03f7b15bfb8b290ae6f271971effa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc026f170c55f97e5257e101243f452",
+    "url": "/opyn-liquidator/index.html"
+  },
+  {
+    "revision": "adc6baeb6d179473e64d",
+    "url": "/opyn-liquidator/static/css/main.b376de7d.chunk.css"
+  },
+  {
+    "revision": "5673d437dfff50e65386",
+    "url": "/opyn-liquidator/static/js/2.125dc7d4.chunk.js"
+  },
+  {
+    "revision": "d947e9197e81c62a781b7cd7b142a028",
+    "url": "/opyn-liquidator/static/js/2.125dc7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc6baeb6d179473e64d",
+    "url": "/opyn-liquidator/static/js/main.603f51c2.chunk.js"
+  },
+  {
+    "revision": "afdb9c46cd42f1e50681",
+    "url": "/opyn-liquidator/static/js/runtime-main.784a42f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opyn-liquidator/precache-manifest.9a64a416118148a0135fa294db6e5800.js"
+  "/opyn-liquidator/precache-manifest.6ae03f7b15bfb8b290ae6f271971effa.js"
 );
 
 self.addEventListener('message', (event) => {
