fa31a702439a4f362c936c14e41c8aff4e370acf
Anton Cheng <anton@coolbitx.com>
2020-03-04 15:08:23 +0800

    Updates

---
+++ precache-manifest.275965c98c00366c42668da3889d5e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33d70af6b233144128bf645fc0b31be",
+    "url": "/opyn-liquidator/index.html"
+  },
+  {
+    "revision": "6e42c218f86c7f696615",
+    "url": "/opyn-liquidator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "688cb769c35f7b77d3c0",
+    "url": "/opyn-liquidator/static/js/2.e4927546.chunk.js"
+  },
+  {
+    "revision": "d947e9197e81c62a781b7cd7b142a028",
+    "url": "/opyn-liquidator/static/js/2.e4927546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e42c218f86c7f696615",
+    "url": "/opyn-liquidator/static/js/main.214f961a.chunk.js"
+  },
+  {
+    "revision": "afdb9c46cd42f1e50681",
+    "url": "/opyn-liquidator/static/js/runtime-main.784a42f0.js"
+  }
+]);
