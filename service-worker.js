634ec1fa8495d2c0b1c5855364fac2796d4534de
antoncoding <antonassocareer@gmail.com>
2020-05-13 01:29:42 +0000

    Deploying to gh-pages from  @ 72fec58a6b384d07b1c04d86c224f7326d28dc1e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e03a04f3be172c681793f3c55cb8e2e8.js"
+  "/precache-manifest.cdd41402615a9a79f51cab715c718064.js"
 );
 
 self.addEventListener('message', (event) => {
